5306193125b5fe82d85fad59e49e39b3142801d1
alexprimak58 <alexprimak998@gmail.com>
2024-05-21 23:34:31 +0300

    just preps

---
+++ frontend/src/App.tsx
@@ -0,0 +1,16 @@
+import React from 'react';
+import './App.css';
+import Footer from './fonts/Components/Footer';
+import LandingPart from './fonts/Components/LandingPart';
+
+function App() {
+  return (
+    <div className="App">
+      <Footer />
+      <LandingPart />
+      <LandingPart />
+    </div>
+  );
+}
+
+export default App;
---
+++ frontend/src/fonts/Components/LandingPart.tsx
@@ -0,0 +1,11 @@
+import React from 'react'
+import LangindgImg from "../../images/LandingImg.png"
+function LandingPart() {
+  return (
+    <div>
+      <img src={LangindgImg} alt="" />
+    </div>
+  )
+}
+
+export default LandingPart
