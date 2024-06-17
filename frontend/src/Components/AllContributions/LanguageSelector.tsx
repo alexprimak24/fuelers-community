011149847c5460ab47557eef8d2e36d90f1e621e
alexprimak58 <aprimak43@gmail.com>
2024-06-18 02:06:15 +0300

    language dropdown + removed unused dependencies

--- frontend/src/Components/AllContributions/LanguageSelector.tsx
+++ frontend/src/Components/AllContributions/LanguageSelector.tsx
@@ -1,4 +1,4 @@
-import React from "react";
+import React, { useState } from "react";
 import styled from "@emotion/styled";
 import { lightColors, darkColors } from "@fuel-ui/css";
 import MenuItem from "@mui/material/MenuItem";
@@ -7,9 +7,9 @@ import InputLabel from "@mui/material/InputLabel/InputLabel";
 import Select from "@mui/material/Select";
 
 const LanguagesVariants = [
-  "testnet",
-  "beta-5",
-  "beta-4",
+  "All",
+  "EN",
+  "CN",
   "beta-3",
   "beta-2",
   "beta-1",
@@ -23,38 +23,52 @@ interface ExampleMenuItem {
   style?: React.CSSProperties;
 }
 
-const StyledFormControl = styled(FormControl)<{ theme: string }>`
+const StyledFormControl = styled(FormControl) <{ theme: string }>`
   ${(props) =>
     props.theme === "dark" &&
     `
-    & fieldset {
+   & fieldset {
       border: none;
     }
     .MuiInputBase-root {
       background-color: ${darkColors.gray1};
-      color: ${lightColors.gray1};
-      outline: 1px solid ${darkColors.gray8};
+      color: ${lightColors.scalesGreen7};
+      border-bottom: 1px solid ${lightColors.scalesGreen7};
+      border-right: 1px solid ${lightColors.scalesGreen7};
+
       &:hover {
-        background: transparent;
+        background: black;
       }
     }
     .MuiFormLabel-root {
-      color: white;
+      color: #E0FFFF;
     }
     .MuiSvgIcon-root {
-      color: ${lightColors.gray8};
+      color: ${lightColors.scalesGreen7};
     }
   `}
 `;
 
 function LanguageSelector() {
+  //add TYPES THERE
+  // onst LanguagesVariants = [
+  //   "All",
+  //   "EN",
+  //   "CN",
+  //   "beta-3",
+  //   "beta-2",
+  //   "beta-1",
+  //   "latest",
+  //   "nightly",
+  // ] as const;
+  // export type Languages = (typeof LanguagesVariants)[number];
+  const [language, setLanguage] = useState('All');
   return (
-    <div className="mx-[140px] max-h-[100px]">
-      <div className="flex text-center justify-between w-full max-h-[100px] rounded-[30px] border border-solid border-defaultwhite px-[50px] pt-[25px]">
+    <div className="mx-[140px] max-h-[100px] mt-[200px]">
+      <div className="flex text-center justify-between w-full max-h-[100px] rounded-[30px] border border-solid border-defaultwhite px-[50px] py-[25px]">
         <p className="text-[32px]">Best work of all time</p>
         {/* CHANGE IT WHEN TIME ONCE DOING DAKR THEME */}
         <StyledFormControl size="small" theme={"dark"}>
-          <InputLabel id="example-select-label">All</InputLabel>
           <span>
             <Select
               MenuProps={{
@@ -68,11 +82,12 @@ function LanguageSelector() {
               id="toolchain-select"
               labelId="toolchain-select-label"
               label="Toolchain"
-              style={{ minWidth: "80px" }}
+              style={{ minWidth: "80px", minHeight: "50px",fontSize:"24px" }}
               variant="outlined"
-              // onChange={(event) =>
-              //   setToolchain(event.target.value as Toolchain)
-              // }
+              value={language}
+              onChange={(event) =>
+                setLanguage(event.target.value)
+              }
             >
               {LanguagesVariants.map((language) => (
                 <MenuItem key={language} value={language}>
